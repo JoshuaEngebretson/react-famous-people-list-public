1c87363ed1b518cc415632f8b51aba9761334311
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-25 17:16:08 -0500

    Completed the last 3 required features
    - Added value to input lines so they can be reset on submit.

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -30,8 +30,10 @@ function FamousSection() {
   const addPerson = (evt) => {
     evt.preventDefault();
     console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
-    
+
     // TODO: create POST request to add this new person to the database
+    // HINT: the server is expecting a person object 
+    //       with a `name` and a `role` property
     axios({
       method: 'POST',
       url: '/people',
@@ -41,14 +43,11 @@ function FamousSection() {
       }
     }).then((reponse) => {
       fetchPeople();
-
+      setPersonName('')
+      setPersonRole('')
     }).catch((error) => {
-
+      console.log('Error POST /people:', error);
     })
-
-    // HINT: the server is expecting a person object 
-    //       with a `name` and a `role` property
-  
   }
 
     return (
@@ -57,11 +56,13 @@ function FamousSection() {
           <label htmlFor="name-input">Name:</label>
           <input 
             id="name-input"
+            value={famousPersonName}
             onChange={e => setPersonName(e.target.value)}
           />
           <label htmlFor="role-input">Famous for:</label>
           <input 
             id="role-input"
+            value={famousPersonRole}
             onChange={e => setPersonRole(e.target.value)}
           />
           <button type="submit">Done</button>
